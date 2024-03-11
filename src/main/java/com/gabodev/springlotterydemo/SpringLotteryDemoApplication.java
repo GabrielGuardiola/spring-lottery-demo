e5e3f4b31c0fe8c398ea0827de9db4fefae5952a
Gabodev <51827577+Gguardiola@users.noreply.github.com>
2024-03-11 12:07:11 +0100

    Initial commit

---
+++ src/main/java/com/gabodev/springlotterydemo/SpringLotteryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gabodev.springlotterydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLotteryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLotteryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabodev/springlotterydemo/SpringLotteryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabodev.springlotterydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLotteryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
